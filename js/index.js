eb0ada598cebc663f6b7ffdb66321abe0ecccea0
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2023-11-13 20:28:43 -0500

    dia2.3

--- js/index.js
+++ js/index.js
@@ -4,6 +4,14 @@ const add = document.querySelector("#btnAdd")
 
 const subtract = document.querySelector("#btnSubtract")
 
+const bar = document.querySelector("#siderbar")
+
+const openMenu = document.querySelector("#btnOpenMenu")
+
+const closeMenu = document.querySelector("#btnCloseMenu")
+
+
+
 add.onclick = function () {
     counter.textContent=Number(counter.textContent)+1
 }
@@ -12,6 +20,15 @@ subtract.onclick = function () {
     counter.textContent=Number(counter.textContent)-1
 }
 
-console.log(counter.textContent);
+openMenu.onclick = function () {
+    bar.classList.remove("hidden")
+    console.log(menu)
+}
 
-console.log(add);
+closeMenu.onclick = function () {
+    bar.classList.add("hidden")
+}
+
+console.log(closeMenu);
+
+console.log(openMenu);
